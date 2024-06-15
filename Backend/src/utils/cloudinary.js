6e6cc3307376d031394ed03ac8d66d26d7482f42
Harsh Agarwal <harshagarwal19696@gmail.com>
2024-06-15 05:43:30 +0530

    updated

--- Backend/src/utils/cloudinary.js
+++ Backend/src/utils/cloudinary.js
@@ -10,8 +10,6 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
-
 const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null;
@@ -21,14 +19,7 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "raw",  // Use raw for files like PDFs
             format: "pdf"
         });
-
-        // If file has been uploaded successfully
-        // console.log("Cloudinary Response:", response);
         console.log("File is uploaded on Cloudinary");
-
-        // Delete the local file after uploading
-        //fs.unlinkSync(localFilePath);
-
         return response;
     } catch (error) {
         // Remove the locally saved temporary file if upload operation failed
